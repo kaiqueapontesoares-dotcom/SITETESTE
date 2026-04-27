48313fa04edd95ff1f05949b1058506c0256fed2
kaiqueapontesoares-dotcom <kaique.aponte.soares@escola.pr.gov.br>
2026-04-27 14:42:03 +0000

    salvei27/4

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botao = document.getElementById("meuBotao");
+
+    botao.addEventListener("click", function() {
+        alert("aaah");
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
